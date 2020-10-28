6714ba7dc57341b165e40f7ad92aba8a89babe85
RVargas <randall.vargas@iica.int>
2020-10-28 13:26:55 -0600

    Agrego página home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import Header from './Components/Header';
 import Intro from './Components/Intro';
 import logo from './logo.svg';
+import Home from './Pages/Home';
+import "./App.css"
 
 function App() {
   return (
     <div>
       <Header />
       <Intro /> 
+      <Home />
     </div>
   );
 }

4826345301cd4cde543ef1a5a93347c4f457350d
RVargas <randall.vargas@iica.int>
2020-11-05 09:13:36 -0600

    Ajuste

--- src/Components/SectionHeader.js
+++ src/Components/SectionHeader.js
@@ -1,10 +1,20 @@
 import "./SectionHeader.css"
 
-const SectionHeader = ({ text }) => {
+const SectionHeader = ({ text, children}) => {
+
+    const printDescription = (description) => {
+      if(description) {
+        return (
+          <p className="section-description">{ description }</p>
+        )
+      }      
+    } 
+    
     return (
         <div class="section-header">
           <h3 class="section-title">{text}</h3>
-          <span class="section-divider"></span>        
+          <span class="section-divider"></span>    
+          { printDescription(children) }   
         </div>
     )
 }

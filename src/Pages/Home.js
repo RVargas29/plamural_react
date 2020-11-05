a534fa5f8ac9e582c58fc6d247e60149bb3a216f
RVargas <randall.vargas@iica.int>
2020-11-05 09:13:47 -0600

    Agrego sección de MoreFeatures

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 const { default: About } = require("../Components/About")
 const { default: Features } = require("../Components/Features")
+const { default: MoreFeautres } = require("../Components/MoreFeatures")
 
 const Home = () => {
     return(
         <main>
             <About />
             <Features />
+            <MoreFeautres />
         </main>
     )
 }

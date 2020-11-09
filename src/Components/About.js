14911f77201ed5c543cd840ceaa4a908ba3169dc
RVargas <randall.vargas@iica.int>
2020-11-09 13:11:21 -0600

    Ajuste a classname

--- src/Components/About.js
+++ src/Components/About.js
@@ -16,7 +16,7 @@ const AboutPanel = ({ title, collapseVariable, collapseHandler, children }) => {
             <div className="panel-heading">
                 <h4 className="panel-title">
                     <a href="#collapse1" onClick={ () => handleCollapse(!collapseVariable) }>
-                        <div style={{color:"cadetblue"}}><i class="far fa-check-square"></i> {title}</div>
+                        <div style={{color:"cadetblue"}}><i className="far fa-check-square"></i> {title}</div>
                     </a>
                 </h4>
             </div>

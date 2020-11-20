40db3dc192c443595e6d4e3ca39872cb3ec01327
RVargas <randall.vargas@iica.int>
2020-11-20 16:08:02 -0600

    Acordeon

--- src/Components/RecordModal.js
+++ src/Components/RecordModal.js
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import { Button, ButtonGroup, Col, Modal, Row } from "react-bootstrap";
+import { Accordion, Button, ButtonGroup, Card, Col, Modal, Row } from "react-bootstrap";
 import GenericField from "./Generic/GenericField";
 import PNButtonGroup from "./Generic/PNButtonGroup";
 import "./RecordModal.css";
@@ -48,21 +48,54 @@ const RecordModal = ({record, recordIndex, show, recordCount, handleClose, handl
                     <Modal.Title>{ record.nombre }</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <GenericField label="Forma de acceso">{record.acceso}</GenericField>
-                    <GenericField label="Institucion">{record.institucion}</GenericField>
-                    <GenericField label="Año de inicio">{record.anno}</GenericField>
-                    <GenericField label="Referente de registro">{record.referente}</GenericField>
-                    <GenericField label="Correo referente" email="true">{record.contacto}</GenericField>
-                    <GenericField label="Link">{record.link}</GenericField>   
-                    <hr/>
-                    <GenericField label="Linea de acción" inline="false">{record.linea_accion}</GenericField>           
-                    <GenericField label="Descripción general" html="true" inline="false">{record.descripcion}</GenericField>      
-                    <GenericField label="Objetivos" html="true" inline="false">{record.objetivo}</GenericField>                    
-                    <GenericField label="Población objetivo" html="true" inline="false">{record.poblacion_objetivo}</GenericField>                    
-                    <GenericField label="Razones de interés" html="true" inline="false">{record.razon_interes}</GenericField>
-                    <GenericField label="Evaluación de impacto" html="true" inline="false">{record.evaluacion_impacto}</GenericField>
-                    <hr/>
-                    <AlcanceTable subsidios_produccion={record.subsidios_produccion} credito={record.credito} asistencia_tecnica={record.asistencia_tecnica} capacitacion={record.capacitacion} />
+                <Accordion defaultActiveKey="0">
+                    <Card>
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                Información general
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="0">
+                        <Card.Body>
+                            <GenericField label="Forma de acceso">{record.acceso}</GenericField>
+                            <GenericField label="Institucion">{record.institucion}</GenericField>
+                            <GenericField label="Año de inicio">{record.anno}</GenericField>
+                            <GenericField label="Referente de registro">{record.referente}</GenericField>
+                            <GenericField label="Correo referente" email="true">{record.contacto}</GenericField>
+                            <GenericField label="Link">{record.link}</GenericField>   
+                        </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                            Detalle
+                        </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="1">
+                        <Card.Body>
+                            <GenericField label="Linea de acción" inline="false">{record.linea_accion}</GenericField>           
+                            <GenericField label="Descripción general" html="true" inline="false">{record.descripcion}</GenericField>      
+                            <GenericField label="Objetivos" html="true" inline="false">{record.objetivo}</GenericField>                    
+                            <GenericField label="Población objetivo" html="true" inline="false">{record.poblacion_objetivo}</GenericField>                    
+                            <GenericField label="Razones de interés" html="true" inline="false">{record.razon_interes}</GenericField>
+                            <GenericField label="Evaluación de impacto" html="true" inline="false">{record.evaluacion_impacto}</GenericField>
+                        </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="3">
+                            Alcance
+                        </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="3">
+                        <Card.Body>
+                            <AlcanceTable subsidios_produccion={record.subsidios_produccion} credito={record.credito} asistencia_tecnica={record.asistencia_tecnica} capacitacion={record.capacitacion} />
+                        </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>                                        
                 </Modal.Body>
                 <Modal.Footer>
                     <PNButtonGroup index={recordIndex} count={recordCount} handlePreviousNext={handlePreviousNext} />

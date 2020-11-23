e4c78dbf7e736e696de3f9a831f633214c8da968
RVargas <randall.vargas@iica.int>
2020-11-23 10:24:06 -0600

    Ajusto eventos

--- src/Components/EventModal.js
+++ src/Components/EventModal.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { Button, ButtonGroup, Col, Modal, Row } from "react-bootstrap";
+import { Accordion, Button, ButtonGroup, Card, Col, Modal, Row } from "react-bootstrap";
 import GenericField from "./Generic/GenericField";
 import PNButtonGroup from "./Generic/PNButtonGroup";
-import "./RecordModal.css";
+import "./EventModal.css";
 
 const EventModal = ({ event, eventIndex, show, eventCount, handleClose, handlePreviousNext }) => {
     
@@ -11,12 +11,53 @@ const EventModal = ({ event, eventIndex, show, eventCount, handleClose, handlePr
     
     if(show && event) {
         return (
-            <Modal show={show} onHide={handleClose} size="lg">
+            <Modal show={show} onHide={handleClose} size="lg" className="info-modal">
                 <Modal.Header closeButton>
                     <Modal.Title>{ event.title }</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <GenericField label="Descripción" html="true" inline="false">{event.descripcion}</GenericField>  
+                    <Accordion defaultActiveKey="0">
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                    <i class="fas fa-info-circle"></i> Información general
+                                </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                                <Card.Body>
+                                    <GenericField label="Descripción" html="true" inline="false">{event.descripcion}</GenericField>  
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                                    <i class="fas fa-file"></i> Presentaciones y documentos
+                                </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="1">
+                                <Card.Body>
+                                    <GenericField label="Presentaciones" html="true" inline="false">{event.presentaciones}</GenericField>  
+                                    <GenericField label="Programa" inline="true"><a href={ "http://plamurural.local" + event.programa } target="_blank">Descargar programa</a></GenericField>  
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                                    <i class="fas fa-video"></i> Video / Transmisión
+                                </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="2">
+                                <Card.Body>
+                                    <div className="video-embed-container">
+                                        <GenericField label="Transmisión / Video" html="true" inline="false">{event.video}</GenericField>  
+                                    </div>
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                    </Accordion>
+                    
                 </Modal.Body>
                 <Modal.Footer>
                     <PNButtonGroup index={eventIndex} count={eventCount} handlePreviousNext={handlePreviousNext} />

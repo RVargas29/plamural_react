32051f4eade18b7cee18373cb0d51c5d89e84b5e
RVargas <randall.vargas@iica.int>
2020-11-24 09:07:57 -0600

    Ajuste a titulo

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -7,7 +7,8 @@ const Intro = () => {
     return(
         <section id="intro">
             <div className="intro-text">
-                <h2>Plataforma Mujeres Rurales Emprendedoras</h2>                
+                <h2>Plataforma de Mujeres Rurales Emprendedoras</h2>                
+                <h2>del MERCOSUR Ampliado</h2>
             </div>
             <div className="product-screens">
                 <div className="product-screen-left wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="0.6s" style={{ visibility: "visible", animationDuration: "0.6s", animationDelay: "0.4s", animationName: "fadeInUp" }}>

3189a652e53704d28ab3f1075434d21b7cb813ad
RVargas <randall.vargas@iica.int>
2020-11-26 09:57:49 -0600

    Ajusto menu

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,41 +6,15 @@ const Header = () => {
         <header id="header">
           <Container>
             <Navbar bg="light" expand="lg">
-              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse>
-                <Nav
-                  activeKey="/home"
-                  className="nav-menu"
-                >
-                  <Nav.Item>
-                    <Nav.Link href="/home">Inicio</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="link-1">¿Qué es la plataforma?</Nav.Link>
-                  </Nav.Item>
-                  <NavDropdown title="Gobernanza">
-                    <Nav.Link eventKey="link-2">La REAF</Nav.Link>
-                    <Nav.Link eventKey="link-2">El IICA</Nav.Link>
-                    <Nav.Link eventKey="link-2">Comisión Directiva</Nav.Link>
-                    <Nav.Link eventKey="link-2">Grupo de Trabajo por país</Nav.Link>
-                  </NavDropdown>
-                  <NavDropdown title="Nosotras en los países">
-                    <Nav.Link eventKey="link-2">Argentina</Nav.Link>
-                    <Nav.Link eventKey="link-2">Brasil</Nav.Link>
-                    <Nav.Link eventKey="link-2">Chile</Nav.Link>
-                    <Nav.Link eventKey="link-2">Colombia</Nav.Link>
-                    <Nav.Link eventKey="link-2">Paraguay</Nav.Link>
-                    <Nav.Link eventKey="link-2">Uruguay</Nav.Link>
-                  </NavDropdown>
-                  <NavDropdown title="Nosotras en los países">
-                    <Nav.Link eventKey="link-2">Marco Normative de MERCOSUR</Nav.Link>
-                    <Nav.Link eventKey="link-2">Líneas de trabajo de género REAF</Nav.Link>
-                    <Nav.Link eventKey="link-2">Foros IICA</Nav.Link>
-                    <Nav.Link eventKey="link-2">Publicaciones</Nav.Link>
-                    <Nav.Link eventKey="link-2">Galería</Nav.Link>
-                    <Nav.Link eventKey="link-2">Uruguay</Nav.Link>
-                  </NavDropdown>
+              <Navbar.Collapse id="main-navbar">
+                <Nav className="mr-auto">
+                  <Nav.Link href="/">Inicio</Nav.Link>
+                  <Nav.Link href="/#about">¿Qué es la plataforma?</Nav.Link>
+                  <Nav.Link href="/#features">Políticas Públicas</Nav.Link>
+                  <Nav.Link href="/gobernanza">Gobernanza</Nav.Link>
+                  <Nav.Link href="/repository">Biblioteca</Nav.Link>
+                  <Nav.Link href="#event-list">Eventos</Nav.Link>
                 </Nav>
               </Navbar.Collapse>
             </Navbar>

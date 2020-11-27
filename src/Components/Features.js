6bb0192b6af524e5e30f2075bdd50eb2e3ace55c
RVargas <randall.vargas@iica.int>
2020-11-27 13:20:42 -0600

    Ajusto enlace

--- src/Components/Features.js
+++ src/Components/Features.js
@@ -3,13 +3,12 @@ import  SectionHeader  from "./SectionHeader";
 
 import "./Features.css"
 import features from "../static/images/features.jpg";
-import { Link } from 'react-router-dom';
 
 const FeatureBox = ({ iconClass, title, link="/",children }) => {
     return (
         <Col lg="4" md="6" className="feature-container wow fadeInRight">
             <div className="icon"><i className={"fas " + iconClass}></i></div>
-            <h4 className="title"><Link to={link}>{title}</Link></h4>
+            <h4 className="title"><a href={link}>{title}</a></h4>
             <p className="description">
                 {children}
             </p>
@@ -31,13 +30,13 @@ const Features = () => {
                     </Col>     
                     <Col lg="8" md="7">
                         <Row>
-                            <FeatureBox iconClass="fa-tachometer-alt" title="Mujer rural" link="/records/sobre">
+                            <FeatureBox iconClass="fa-tachometer-alt" title="Mujer rural" link="/records/sobre#anchor-record-list">
                                 Se detallan las diferentes políticas públicas impulsadas y en implementación en los países que tienen como foco de atención las mujeres rurales de la Agricultura Familiar
                             </FeatureBox>
-                            <FeatureBox iconClass="fa-flask" title="Emprendimientos y apoyo a la producción" link="/records/emprendimientos">
+                            <FeatureBox iconClass="fa-flask" title="Emprendimientos y apoyo a la producción" link="/records/emprendimientos#anchor-record-list">
                             Lista las políticas públicas, programas, proyectos y otros instrumentos de políticas que incentivan emprendimientos, negocios o actividades productivas con fines comerciales.
                             </FeatureBox>
-                            <FeatureBox iconClass="fa-flask" title="Apoyo a la producción para mujer rural" link="/records/emprendeurismo">
+                            <FeatureBox iconClass="fa-flask" title="Apoyo a la producción para mujer rural" link="/records/emprendeurismo#anchor-record-list">
                             Sistematiza las políticas públicas que incentivan emprendimientos liderados o implementados por mujeres rurales y su inclusión a dinámicas productivas.
                             </FeatureBox>
                         </Row>
